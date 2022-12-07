5973f5450ae068b2b142b8761b435ab3a3916a10
Evan Rosson <rossonevan7281@gmail.com>
2022-12-07 10:20:33 -0600

    Added number buttons

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header >
-        
-      </header>
+      <div>
+        <button onClick={e => console.log(e.target.value)} value={1}>1</button>
+        <button onClick={e => console.log(e.target.value)} value={2}>2</button>
+        <button onClick={e => console.log(e.target.value)} value={3}>3</button>
+        <br></br>
+        <button onClick={e => console.log(e.target.value)} value={4}>4</button>
+        <button onClick={e => console.log(e.target.value)} value={5}>5</button>
+        <button onClick={e => console.log(e.target.value)} value={6}>6</button>
+        <br></br>
+        <button onClick={e => console.log(e.target.value)} value={7}>7</button>
+        <button onClick={e => console.log(e.target.value)} value={8}>8</button>
+        <button onClick={e => console.log(e.target.value)} value={9}>9</button>
+        <br></br>
+        <button onClick={e => console.log(e.target.value)} value={0}>0</button>
+      </div>
     </div>
   );
 }
