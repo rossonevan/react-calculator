630e680e1a2720224e92223e8235bfa27403edd8
Evan Rosson <rossonevan7281@gmail.com>
2022-12-07 10:31:34 -0600

    Added state for input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [calc, setCalc] = useState(0);
+
+
+  const number = (e) => {
+    setCalc(e.target.value)
+  }
+
+
+
+
   return (
     <div className="App">
       <div>
-        <button onClick={e => console.log(e.target.value)} value={1}>1</button>
-        <button onClick={e => console.log(e.target.value)} value={2}>2</button>
-        <button onClick={e => console.log(e.target.value)} value={3}>3</button>
+        <h1>{calc}</h1>
+        <button onClick={number} value={1}>1</button>
+        <button onClick={number} value={2}>2</button>
+        <button onClick={number} value={3}>3</button>
         <br></br>
-        <button onClick={e => console.log(e.target.value)} value={4}>4</button>
-        <button onClick={e => console.log(e.target.value)} value={5}>5</button>
-        <button onClick={e => console.log(e.target.value)} value={6}>6</button>
+        <button onClick={number} value={4}>4</button>
+        <button onClick={number} value={5}>5</button>
+        <button onClick={number} value={6}>6</button>
         <br></br>
-        <button onClick={e => console.log(e.target.value)} value={7}>7</button>
-        <button onClick={e => console.log(e.target.value)} value={8}>8</button>
-        <button onClick={e => console.log(e.target.value)} value={9}>9</button>
+        <button onClick={number} value={7}>7</button>
+        <button onClick={number} value={8}>8</button>
+        <button onClick={number} value={9}>9</button>
         <br></br>
-        <button onClick={e => console.log(e.target.value)} value={0}>0</button>
+        <button onClick={number} value={0}>0</button>
+        <br></br>
+        <button>+</button>
+        <button>-</button>
       </div>
     </div>
   );
